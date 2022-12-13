198c69139bbcc83b7fd093a8e2cc6ca236d7172c
paminicha <pamnichapatr@gmail.com>
2022-12-13 16:29:42 +0100

    add class

--- TKinterModel/main_scene.py
+++ TKinterModel/main_scene.py
@@ -1,18 +1,29 @@
-from tkinter import *
-
-window = Tk()
-window.geometry('650x550')
-window.attributes('-fullscreen', True)
-
-# label Title
-label = Label(text="Local Library", font=('Times New Roman bold', 20), background="#34A2FE")
-label.pack(padx=10, pady=10)
-
-# Create a Button
-btn = Button(window, text='Click me !', bd='5',
-             command=window.destroy)
-# Set the position of button on the top of window.
-btn.pack(side='top')
+import tkinter as tk
+from tkinter.messagebox import showinfo
 
 
-window.mainloop()
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        # configure the root window
+        self.title('Local Library Application')
+        self.geometry('650x550')
+        self.attributes('-fullscreen', True)
+
+        # label Title
+        self.label = tk.Label(text="Local Library", font=('Times New Roman bold', 20), background="#34A2FE")
+        self.label.pack(padx=10, pady=10)
+
+        # Create a Button
+        self.btn = tk.Button(self, text='Click me !', bd='5')
+        self.btn['command'] = self.buttton_clicked
+        self.btn.pack(side='top')
+
+    def buttton_clicked(self):
+        showinfo(showinfo='information', message='hello')
+
+
+if __name__ == "__main__":
+  app = App()
+  app.mainloop()
