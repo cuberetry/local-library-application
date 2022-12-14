8671ac1226035fa78319c7490c76ff8851ea8fb6
Tanapol Dangsakul <tanapol.dgsk@gmail.com>
2022-12-14 17:44:53 +0100

    Added Python SQL executor script in SQLScript/execute_sql.py and fixed issue with db_init.sql

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
+import os
 import mysql.connector
 from TKinterModel.main_scene import *
+import SQLScript.execute_sql
 
 if __name__ == "__main__":
     # Establish connection to the database
     connection = mysql.connector.connect(
         host="localhost",
         port="3306",
-        user='root',
-        database="Local_Library_Schema"
+        user="root"
     )
     cursor = connection.cursor()
+    SQLScript.execute_sql.execute_sql("./SQLScript/db_init.sql", cursor)
+
     # Remove logs in later version
-    print(connection)
-    cursor.execute("SHOW TABLES")
-    for table in cursor:
-        print(table)
+    # print(connection)
+    # cursor.execute("SHOW TABLES")
+    # for table in cursor:
+    #     print(table)
 
     app = App()
     app.mainloop()
