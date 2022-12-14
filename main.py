c22e439b3970f69fdd4e03827be982f9ccca6848
Tanapol Dangsakul <tanapol.dgsk@gmail.com>
2022-12-14 15:21:59 +0100

    Added DB connection and cursor

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import tkinter
-import sqlite3
+import mysql.connector
 from TKinterModel.main_scene import *
 
 if __name__ == "__main__":
+    # Establish connection to the database
+    connection = mysql.connector.connect(
+        host="localhost",
+        port="3306",
+        user='root',
+        database="Local_Library_Schema"
+    )
+    cursor = connection.cursor()
+    # Remove logs in later version
+    print(connection)
+    cursor.execute("SHOW TABLES")
+    for table in cursor:
+        print(table)
+
     app = App()
     app.mainloop()
