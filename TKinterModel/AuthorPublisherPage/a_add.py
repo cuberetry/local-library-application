9c27122b9b589a9333782debd52f1f18cbf6d183
flukekd <flukekd@gmail.com>
2022-12-19 18:09:12 +0100

    Insert improvement
    Clear entry field after submit changes
    Text indication when insert command is executed

--- TKinterModel/AuthorPublisherPage/a_add.py
+++ TKinterModel/AuthorPublisherPage/a_add.py
@@ -4,7 +4,17 @@ import TKinterModel.SystemPage.sys_home_page as sh
 import TKinterModel.AuthorPublisherPage.ap_main as apm
 import __main__ as m
 
+def set_text_by_button(fn, ln, fn_entry, ln_entry, finish_label):
+    # insert to SQL
+    m.sql_connection.sql_insert('AUTHOR', {'a_fname':fn.get(),'a_lname':ln.get()})
 
+    #Empty input field
+    fn_entry.delete(0,"end")
+    ln_entry.delete(0,"end")
+
+    #Indicate successfull insert
+    finish_label.config(text="Added successfully")
+    
 class AuthorAddPage(tk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
@@ -33,7 +43,10 @@ class AuthorAddPage(tk.Frame):
         author_ln_entry = tk.Entry(self, textvariable=author_ln)
         author_ln_entry.pack(padx=10, pady=2)
 
-        submit_button=tk.Button(self ,text = 'Submit', command=lambda: m.sql_connection.sql_insert('AUTHOR', {'a_fname':author_fn.get(),'a_lname':author_ln.get()}))
+        success_label = tk.Label(self, text="")
+        success_label.pack(padx=20, pady=20)
+
+        submit_button=tk.Button(self ,text = 'Submit', command=lambda:set_text_by_button(author_fn, author_ln, author_fn_entry, author_ln_entry, success_label))
         submit_button.pack(padx=10, pady=20)
 
 
