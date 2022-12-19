5effaf982eee84f0727e8bbd4849864a00a146bb
paminicha <pamnichapatr@gmail.com>
2022-12-19 16:33:58 +0100

    add entry

--- TKinterModel/AuthorPublisherPage/p_add.py
+++ TKinterModel/AuthorPublisherPage/p_add.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import TKinterModel.SystemPage.sys_frame as sf
 import TKinterModel.SystemPage.sys_home_page as sh
 import TKinterModel.AuthorPublisherPage.ap_main as apm
+import __main__ as m
 
 
 class PublisherAddPage(tk.Frame):
@@ -16,3 +17,15 @@ class PublisherAddPage(tk.Frame):
 
         label = tk.Label(self, text="This is publisher add page")
         label.pack(padx=10, pady=20)
+
+        p_name_l = tk.Label(self, text='Publisher Name: ')
+        p_name_l.pack(padx=10, pady=10)
+        p_name_e = tk.Entry(self)
+        p_name_e.pack(padx=10, pady=10)
+
+        p_name = p_name_e.get()
+        m.sql_connection.sql_insert("PUBLISHERS", {"b_name": "Harry Potter Vol.1", "b_desc": "Test"})
+
+
+
+
