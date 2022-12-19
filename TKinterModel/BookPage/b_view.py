67cdbec0a09a77a9c0916a53633908c1d5004014
Tanapol Dangsakul <tanapol.dgsk@gmail.com>
2022-12-19 17:02:10 +0100

    + Added a book list table

--- TKinterModel/BookPage/b_view.py
+++ TKinterModel/BookPage/b_view.py
@@ -1,7 +1,9 @@
 import tkinter as tk
+from tkinter import ttk
 import TKinterModel.SystemPage.sys_frame as sf
 import TKinterModel.SystemPage.sys_home_page as sh
 import TKinterModel.BookPage.b_main as bm
+import __main__ as m
 
 
 class BookViewPage(tk.Frame):
@@ -14,3 +16,18 @@ class BookViewPage(tk.Frame):
 
         label = tk.Label(self, text="This is book view page")
         label.pack(padx=10, pady=20)
+
+        # Init DB connection and table
+        self.book_list = m.sql_connection.sql_select("BOOKS")
+        self.columns = ["ID", "Name", "Description", "Book Status", "Author ID", "Publisher ID"]
+        self.table = ttk.Treeview(self, columns=self.columns, show="headings", height=27)
+        for col in self.columns:
+            self.table.heading(col, text=col.title())
+        self.update_table(0)
+
+    def update_table(self, page=0):
+        for i in range(page*25, (page*25)+25):
+            if i > len(self.book_list)-1:
+                break
+            self.table.insert("", 'end', values=self.book_list[i])
+        self.table.place(x=200, y=270)
