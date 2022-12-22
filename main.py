b1595a0acdc605e87ce1943ecad056aa24d32ed2
paminicha <92308105+paminicha@users.noreply.github.com>
2022-12-22 18:32:02 +0100

    Update main.py
    change from True to False on line 6

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from SQLModel.SQLConnection import *
 
 if __name__ == "__main__":
     # Establish connection to the database
-    sql_connection = SQLConnection("localhost", "3306", "root", "Local_Library_Schema", True)
+    sql_connection = SQLConnection("localhost", "3306", "root", "Local_Library_Schema", False)
 
     # Tkinter mainloop
     app = App()
