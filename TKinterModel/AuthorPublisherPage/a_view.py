4aa1960b8dd30c640bd2264d5a93dcd1940be472
flukekd <flukekd@gmail.com>
2023-01-05 18:15:27 +0100

    add view authors page

--- TKinterModel/AuthorPublisherPage/a_view.py
+++ TKinterModel/AuthorPublisherPage/a_view.py
@@ -1,18 +1,80 @@
 import tkinter as tk
+from tkinter import ttk
 import TKinterModel.SystemPage.sys_frame as sf
 import TKinterModel.SystemPage.sys_home_page as sh
 import TKinterModel.AuthorPublisherPage.ap_main as apm
+import TKinterModel.AuthorPublisherPage.a_edit as ae
+import __main__ as m
 
 
 class AuthorViewPage(tk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
+        self.cur_page = 0
+
         home_button = tk.Button(self, text='Homepage',
                                 command=lambda: sf.show_frame(sh.Homepage))
         home_button.pack(padx=10, pady=20)
-        book_button = tk.Button(self, text='Author/Publisher Page',
-                                command=lambda: sf.show_frame(apm.AuthorPublisherMainPage))
-        book_button.pack(padx=10, pady=20)
+        author_button = tk.Button(self, text='Author/Publisher Page',
+                                  command=lambda: sf.show_frame(apm.AuthorPublisherMainPage))
+        author_button.pack(padx=10, pady=20)
 
-        label = tk.Label(self, text="This is author view page")
-        label.pack(padx=10, pady=20)
+        refresh_button = tk.Button(
+            self, text='Refresh', command=lambda: self.refresh())
+        refresh_button.pack(padx=10, pady=20)
+        next_button = tk.Button(
+            self, text='Next', command=lambda: self.next_page())
+        next_button.pack(padx=0, pady=20, side=tk.RIGHT)
+        prev_button = tk.Button(
+            self, text='Prev', command=lambda: self.prev_page())
+        prev_button.pack(padx=0, pady=20, side=tk.LEFT)
+
+        delete_button = tk.Button(
+            self, text='Delete', command=lambda: self.delete_item())
+        delete_button.pack(padx=0, pady=0, side=tk.BOTTOM)
+        edit_button = tk.Button(
+            self, text='Edit', command=lambda: self.edit_item())
+        edit_button.pack(padx=0, pady=0, side=tk.BOTTOM)
+
+        # Init DB connection and table
+        self.author_list = m.sql_connection.sql_select("AUTHOR")
+        self.columns = ["ID", "Firstname", "Lastname"]
+        self.table = ttk.Treeview(
+            self, columns=self.columns, show="headings", height=27)
+        for col in self.columns:
+            self.table.heading(col, text=col.title())
+        self.update_table()
+
+    def update_table(self):
+        self.author_list = m.sql_connection.sql_select("AUTHOR")
+        self.table.delete(*self.table.get_children())
+        for i in range(self.cur_page*25, (self.cur_page*25)+25):
+            if i > len(self.author_list)-1:
+                break
+            self.table.insert("", 'end', values=self.author_list[i])
+        self.table.pack(fill="both", expand=True, padx=10)
+
+    def refresh(self):
+        self.cur_page = 0
+        self.update_table()
+
+    def next_page(self):
+        if self.cur_page < len(self.author_list)//25:
+            self.cur_page += 1
+        self.update_table()
+
+    def prev_page(self):
+        if self.cur_page > 0:
+            self.cur_page -= 1
+        self.update_table()
+
+    def edit_item(self):
+        cur_item = self.table.item(self.table.focus())
+        if cur_item['values'] != "":
+            sf.frames[ae.AuthorEditPage].target = cur_item
+            sf.show_frame(ae.AuthorEditPage)
+
+    def delete_item(self):
+        cur_item = self.table.item(self.table.focus())
+        m.sql_connection.sql_delete("AUTHOR", cur_item['values'][0])
+        self.refresh()
--- TKinterModel/SystemPage/sys_main_scene.py
+++ TKinterModel/SystemPage/sys_main_scene.py
@@ -8,6 +8,7 @@ import TKinterModel.BookPage.b_edit as b_edit
 import TKinterModel.AuthorPublisherPage.ap_main as ap_main
 import TKinterModel.AuthorPublisherPage.a_view as a_view
 import TKinterModel.AuthorPublisherPage.a_add as a_add
+import TKinterModel.AuthorPublisherPage.a_edit as a_edit
 import TKinterModel.AuthorPublisherPage.a_remove as a_remove
 import TKinterModel.AuthorPublisherPage.p_view as p_view
 import TKinterModel.AuthorPublisherPage.p_add as p_add
@@ -33,7 +34,8 @@ class App(tk.Tk):
         self.attributes('-fullscreen', True)
 
         # label Title
-        self.label = tk.Label(text="Local Library", font=('Times New Roman bold', 20), background="#34A2FE")
+        self.label = tk.Label(text="Local Library", font=(
+            'Times New Roman bold', 20), background="#34A2FE")
         self.label.pack(padx=10, pady=20)
 
         container = tk.Frame(self)
@@ -43,7 +45,7 @@ class App(tk.Tk):
         container.grid_columnconfigure(0, weight=1)
 
         for f in (sh.Homepage, b_main.BookMainPage, b_view.BookViewPage, b_add.BookAddPage, b_edit.BookEditPage,
-                  ap_main.AuthorPublisherMainPage, a_view.AuthorViewPage, a_add.AuthorAddPage,
+                  ap_main.AuthorPublisherMainPage, a_view.AuthorViewPage, a_add.AuthorAddPage, a_edit.AuthorEditPage,
                   a_remove.AuthorRemovePage, p_view.PublisherViewPage, p_add.PublisherAddPage,
                   p_remove.PublisherRemovePage, mb_main.MemberMainPage, mb_view.MemberViewPage,
                   mb_add.MemberAddPage, mb_edit.MemberEditPage, mb_remove.MemberRemovePage,
@@ -63,5 +65,3 @@ class App(tk.Tk):
 
     def button_clicked(self):
         self.destroy()
-
-
