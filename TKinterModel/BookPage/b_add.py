425f9b01b71c74ea58011fbf17b1298acb8a7b9e
flukekd <flukekd@gmail.com>
2023-01-05 15:55:13 +0100

    Edit error msg

--- TKinterModel/BookPage/b_add.py
+++ TKinterModel/BookPage/b_add.py
@@ -16,13 +16,13 @@ class BookAddPage(tk.Frame):
         label = tk.Label(self, text="This is book add page")
         label.pack(padx=10, pady=20)
 
-        self.book_name_label = tk.Label(self, text="Add Book Name")
+        self.book_name_label = tk.Label(self, text="Add Book Name*")
         self.book_name_label.pack(padx=10, pady=2)
 
         self.book_name_entry = tk.Entry(self)
         self.book_name_entry.pack(padx=10, pady=2)
 
-        self.book_des_label = tk.Label(self, text="Add Book Description")
+        self.book_des_label = tk.Label(self, text="Add Book Description (Max 200 Chatacters)")
         self.book_des_label.pack(padx=10, pady=2)
 
         self.book_des_entry = tk.Text(self, width=60, height=4)
@@ -31,28 +31,28 @@ class BookAddPage(tk.Frame):
         self.submit_button=tk.Button(self, text = 'Submit', command=lambda:self.add_to_SQL())
         self.submit_button.pack(padx=10, pady=20)
 
-        self.error_label = tk.Label(self, text="", fg="red")
+        self.error_label = tk.Label(self, text="", fg="IndianRed1")
         self.error_label.pack(padx=10, pady=2)
 
     def add_to_SQL(self):
         bk_name = self.book_name_entry.get()
         bk_des = self.book_des_entry.get("1.0","end")
 
-        # Empty input field
-        self.book_name_entry.delete(0,"end")
-        self.book_des_entry.delete('1.0',"end")
-
         # Check if Book Name is empty
         if len(bk_name) <= 1:
-            self.error_label.config(text="Book name is blank")
+            self.error_label.config(text="Please fill the all the required field(s)")
             return
         # Limit Book Description input characters
         elif len(bk_des) > 201:
-            self.error_label.config(text="Text exceeds 200 characters")
+            self.error_label.config(text="Text exceeded 200 characters")
             return
         # Clear error msg
         else:
             self.error_label.config(text="")
+
+        # Empty input field
+        self.book_name_entry.delete(0,"end")
+        self.book_des_entry.delete('1.0',"end")
 
         # Insert to SQL
         m.sql_connection.sql_insert('BOOKS', {'b_name':bk_name,'b_desc':bk_des, 'a_id':1, 'p_id':1})
