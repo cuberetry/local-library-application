0d5f03f106e3a36517a657f7d645d06e7deb557d
Tanapol Dangsakul <tanapol.dgsk@gmail.com>
2023-01-07 19:46:54 +0100

    Add first time setting

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from TKinterModel.SystemPage.sys_main_scene import *
 from SQLModel.SQLConnection import *
+import user_setting
 
 if __name__ == "__main__":
     # Establish connection to the database
-    sql_connection = SQLConnection("localhost", "3306", "root", "Local_Library_Schema", False)
+    sql_connection = SQLConnection("localhost", "3306", "root", "Local_Library_Schema", user_setting.first_time)
+    f = open("user_setting.py", "w")
+    f.write("first_time = False\n")
+    f.close()
 
     # Tkinter mainloop
     app = App()
