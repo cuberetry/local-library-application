e4bd6aa39c060f503884412bcc9f9ff45a6d6950
Tanapol Dangsakul <tanapol.dgsk@gmail.com>
2023-01-07 20:14:34 +0100

    Added comments in main.py

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ import user_setting
 if __name__ == "__main__":
     # Establish connection to the database
     sql_connection = SQLConnection("localhost", "3306", "root", "Local_Library_Schema", user_setting.first_time)
+
+    # Update first time file
     f = open("user_setting.py", "w")
     f.write("first_time = False\n")
     f.close()
+
+    # Update birthday
+    sql_connection.sql_select("MEMBER")
 
     # Tkinter mainloop
     app = App()
