d6b139e092caf8296dd6fd0c4d714ada9bdac546
Pitchapa Arun <uyuyecy@gmail.com>
2023-01-07 19:42:54 +0100

    link page edit date time error

--- TKinterModel/MemberPage/mb_add.py
+++ TKinterModel/MemberPage/mb_add.py
@@ -2,27 +2,26 @@ import tkinter as tk
 import TKinterModel.SystemPage.sys_frame as sf
 import TKinterModel.SystemPage.sys_home_page as sh
 import TKinterModel.MemberPage.mb_main as mm
+import TKinterModel.MemberPage.mb_view as mv
 import __main__ as m
 import datetime as d
-from datetime import date
 from tkcalendar import DateEntry
-
-
 
 
 class MemberAddPage(tk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
         self.error_msg = ""
-        home_button = tk.Button(self, text='Homepage', command=lambda: sf.show_frame(sh.Homepage))
+        home_button = tk.Button(self, text='Homepage',
+                                command=lambda: sf.show_frame(sh.Homepage))
         home_button.pack(padx=10, pady=20)
-        book_button = tk.Button(self, text='Member Page', command=lambda: sf.show_frame(mm.MemberMainPage))
+        book_button = tk.Button(self, text='Member Page',
+                                command=lambda: sf.show_frame(mm.MemberMainPage))
         book_button.pack(padx=10, pady=10)
-
 
         # self.author_fn_label = tk.Label(self, text="Add Author First Name",width=10,font=("bold", 11))
         # self.author_fn_label.pack(padx=10, pady=2)
-        # self.author_fn_label.place(x=90,y=53)  
+        # self.author_fn_label.place(x=90,y=53)
 
         # self.author_fn_entry = tk.Entry(self)
         # self.author_fn_entry.pack(padx=10, pady=2)
@@ -52,7 +51,8 @@ class MemberAddPage(tk.Frame):
         self.member_bd_entry = DateEntry(self)
         self.member_bd_entry.pack(padx=10, pady=2)
 
-        self.member_phone_label = tk.Label(self, text="Enter member Phone number")
+        self.member_phone_label = tk.Label(
+            self, text="Enter member Phone number")
         self.member_phone_label.pack(padx=10, pady=2)
 
         self.member_phone_entry = tk.Entry(self)
@@ -64,13 +64,15 @@ class MemberAddPage(tk.Frame):
         self.member_email_entry = tk.Entry(self)
         self.member_email_entry.pack(padx=10, pady=2)
 
-        self.member_national_id_label = tk.Label(self, text="Enter member National ID")
+        self.member_national_id_label = tk.Label(
+            self, text="Enter member National ID")
         self.member_national_id_label.pack(padx=10, pady=2)
- 
+
         self.member_national_id_entry = tk.Entry(self)
         self.member_national_id_entry.pack(padx=10, pady=2)
 
-        self.member_passport_id_label = tk.Label(self, text="Enter member Passport ID")
+        self.member_passport_id_label = tk.Label(
+            self, text="Enter member Passport ID")
         self.member_passport_id_label.pack(padx=10, pady=2)
 
         self.member_passport_id_entry = tk.Entry(self)
@@ -82,7 +84,8 @@ class MemberAddPage(tk.Frame):
         self.member_address_entry = tk.Entry(self)
         self.member_address_entry.pack(padx=10, pady=2)
 
-        self.submit_button=tk.Button(self ,text = 'Submit', command=lambda:self.add_to_SQL())
+        self.submit_button = tk.Button(
+            self, text='Submit', command=lambda: self.add_to_SQL())
         self.submit_button.pack(padx=10, pady=2)
 
         # Error message
@@ -118,7 +121,7 @@ class MemberAddPage(tk.Frame):
             self.error_msg = "Age exceeded 3 digits"
             self.error_label.config(text=self.error_msg)
             return
-        
+
         elif mb_age != '' and mb_age.isnumeric() is False:
             self.error_msg = "Please enter a valid age"
             self.error_label.config(text=self.error_msg)
@@ -154,25 +157,30 @@ class MemberAddPage(tk.Frame):
             self.error_label.config(text=self.error_msg)
             return
 
+        mb_birthday = mb_birthday.strftime('%Y-%m-%d')
 
         # Insert to SQL
-        m.sql_connection.sql_insert('MEMBERSß', {'mb_fname':mb_fname,'mb_lname':mb_lname, 'mb_age':mb_age, 'mb_birthday':mb_birthday, 'mb_phone':mb_phone, 'mb_email':mb_email, 'mb_national_id':mb_national_id, 'mb_passport_id':mb_passport_id, 'mb_address':mb_address})
+        m.sql_connection.sql_insert('MEMBERS', {'mb_fname': mb_fname, 'mb_lname': mb_lname, 'mb_age': int(mb_age), 'mb_birthday': mb_birthday,
+                                    'mb_phone': mb_phone, 'mb_email': mb_email, 'mb_national_id': mb_national_id, 'mb_passport_id': mb_passport_id, 'mb_address': mb_address})
 
         # Empty input field
-        self.member_fn_entry.delete(0,"end")
-        self.member_ln_entry.delete(0,"end")
-        self.member_age_entry.delete(0,"end")
-        self.member_bd_entry.delete(0,"end")
-        self.member_phone_entry.delete(0,"end")
-        self.member_email_entry.delete(0,"end")
-        self.member_national_id_entry.delete(0,"end")
-        self.member_passport_id_entry.delete(0,"end")
-        self.member_address_entry.delete(0,"end")
+        self.member_fn_entry.delete(0, "end")
+        self.member_ln_entry.delete(0, "end")
+        self.member_age_entry.delete(0, "end")
+        self.member_bd_entry.delete(0, "end")
+        self.member_phone_entry.delete(0, "end")
+        self.member_email_entry.delete(0, "end")
+        self.member_national_id_entry.delete(0, "end")
+        self.member_passport_id_entry.delete(0, "end")
+        self.member_address_entry.delete(0, "end")
         self.error_msg = ""
-        self.error_label.config(text=self.error_msg)    
+        self.error_label.config(text=self.error_msg)
 
-class RegisterWindow(tk.Toplevel): 
-    def __init__(self, parent): 
-        super().__init__(parent)  
+        # Return user to author page
+        sf.frames[mv.MemberViewPage].refresh()
+        sf.show_frame(mv.MemberViewPage)
 
-      
+
+class RegisterWindow(tk.Toplevel):
+    def __init__(self, parent):
+        super().__init__(parent)
